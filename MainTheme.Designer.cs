8523cf4fab956b212bf58e2d307605de08073808
EurasDev <jackamatic@live.com>
2016-02-02 11:29:37 +0000

    j

---
+++ MainTheme.Designer.cs
@@ -0,0 +1,213 @@
+﻿namespace EasyColourTheme
+{
+    partial class MainTheme
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.eC_Theme1 = new EasyColourTheme.EC_Theme();
+            this.eC_Tabs1 = new EasyColourTheme.EC_Tabs();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.eC_Panel1 = new EasyColourTheme.EC_Panel();
+            this.eC_Notification1 = new EasyColourTheme.EC_Notification();
+            this.eC_Button1 = new EasyColourTheme.EC_Button();
+            this.eC_CheckBox1 = new EasyColourTheme.EC_CheckBox();
+            this.eC_CheckBox2 = new EasyColourTheme.EC_CheckBox();
+            this.eC_ProgressBar1 = new EasyColourTheme.EC_ProgressBar();
+            this.eC_TextBox1 = new EasyColourTheme.EC_TextBox();
+            this.eC_Theme1.SuspendLayout();
+            this.eC_Tabs1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.eC_Panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // eC_Theme1
+            // 
+            this.eC_Theme1.BackColor = System.Drawing.Color.CadetBlue;
+            this.eC_Theme1.BorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.eC_Theme1.Controls.Add(this.eC_Notification1);
+            this.eC_Theme1.Controls.Add(this.eC_Tabs1);
+            this.eC_Theme1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.eC_Theme1.ForeColor = System.Drawing.Color.White;
+            this.eC_Theme1.Location = new System.Drawing.Point(0, 0);
+            this.eC_Theme1.Name = "eC_Theme1";
+            this.eC_Theme1.Size = new System.Drawing.Size(268, 327);
+            this.eC_Theme1.TabIndex = 0;
+            this.eC_Theme1.Text = "eC_Theme1";
+            this.eC_Theme1.Transparent = false;
+            // 
+            // eC_Tabs1
+            // 
+            this.eC_Tabs1.Controls.Add(this.tabPage1);
+            this.eC_Tabs1.Controls.Add(this.tabPage2);
+            this.eC_Tabs1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.eC_Tabs1.DrawMode = System.Windows.Forms.TabDrawMode.OwnerDrawFixed;
+            this.eC_Tabs1.ItemSize = new System.Drawing.Size(100, 20);
+            this.eC_Tabs1.Location = new System.Drawing.Point(0, 65);
+            this.eC_Tabs1.Name = "eC_Tabs1";
+            this.eC_Tabs1.SelectedIndex = 0;
+            this.eC_Tabs1.Size = new System.Drawing.Size(268, 262);
+            this.eC_Tabs1.TabIndex = 1;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.eC_Panel1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 24);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(260, 234);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "tabPage1";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 24);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(276, 72);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "tabPage2";
+            // 
+            // eC_Panel1
+            // 
+            this.eC_Panel1.Controls.Add(this.eC_TextBox1);
+            this.eC_Panel1.Controls.Add(this.eC_ProgressBar1);
+            this.eC_Panel1.Controls.Add(this.eC_CheckBox2);
+            this.eC_Panel1.Controls.Add(this.eC_CheckBox1);
+            this.eC_Panel1.Controls.Add(this.eC_Button1);
+            this.eC_Panel1.Location = new System.Drawing.Point(22, 20);
+            this.eC_Panel1.Name = "eC_Panel1";
+            this.eC_Panel1.PanelText = "eC_Panel1";
+            this.eC_Panel1.Size = new System.Drawing.Size(216, 195);
+            this.eC_Panel1.TabIndex = 1;
+            // 
+            // eC_Notification1
+            // 
+            this.eC_Notification1.BackColor = System.Drawing.Color.Transparent;
+            this.eC_Notification1.Location = new System.Drawing.Point(12, 34);
+            this.eC_Notification1.Name = "eC_Notification1";
+            this.eC_Notification1.Notification = "This is a notification!";
+            this.eC_Notification1.Size = new System.Drawing.Size(200, 25);
+            this.eC_Notification1.TabIndex = 0;
+            this.eC_Notification1.Tag = "";
+            // 
+            // eC_Button1
+            // 
+            this.eC_Button1.BackColor = System.Drawing.Color.Transparent;
+            this.eC_Button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.eC_Button1.Image = null;
+            this.eC_Button1.ImageSize = new System.Drawing.Size(0, 0);
+            this.eC_Button1.Location = new System.Drawing.Point(69, 34);
+            this.eC_Button1.Name = "eC_Button1";
+            this.eC_Button1.Size = new System.Drawing.Size(82, 47);
+            this.eC_Button1.TabIndex = 0;
+            this.eC_Button1.Text = "eC_Button1";
+            // 
+            // eC_CheckBox1
+            // 
+            this.eC_CheckBox1.BackColor = System.Drawing.Color.Transparent;
+            this.eC_CheckBox1.Checked = false;
+            this.eC_CheckBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.eC_CheckBox1.Location = new System.Drawing.Point(69, 87);
+            this.eC_CheckBox1.MaximumSize = new System.Drawing.Size(20, 20);
+            this.eC_CheckBox1.MinimumSize = new System.Drawing.Size(20, 20);
+            this.eC_CheckBox1.Name = "eC_CheckBox1";
+            this.eC_CheckBox1.Size = new System.Drawing.Size(20, 20);
+            this.eC_CheckBox1.TabIndex = 1;
+            this.eC_CheckBox1.Text = "eC_CheckBox1";
+            // 
+            // eC_CheckBox2
+            // 
+            this.eC_CheckBox2.BackColor = System.Drawing.Color.Transparent;
+            this.eC_CheckBox2.Checked = true;
+            this.eC_CheckBox2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.eC_CheckBox2.Location = new System.Drawing.Point(131, 87);
+            this.eC_CheckBox2.MaximumSize = new System.Drawing.Size(20, 20);
+            this.eC_CheckBox2.MinimumSize = new System.Drawing.Size(20, 20);
+            this.eC_CheckBox2.Name = "eC_CheckBox2";
+            this.eC_CheckBox2.Size = new System.Drawing.Size(20, 20);
+            this.eC_CheckBox2.TabIndex = 2;
+            this.eC_CheckBox2.Text = "eC_CheckBox2";
+            // 
+            // eC_ProgressBar1
+            // 
+            this.eC_ProgressBar1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.eC_ProgressBar1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(200)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.eC_ProgressBar1.Location = new System.Drawing.Point(36, 155);
+            this.eC_ProgressBar1.Max = 100;
+            this.eC_ProgressBar1.Min = 0;
+            this.eC_ProgressBar1.Name = "eC_ProgressBar1";
+            this.eC_ProgressBar1.Size = new System.Drawing.Size(150, 24);
+            this.eC_ProgressBar1.TabIndex = 3;
+            this.eC_ProgressBar1.Text = "eC_ProgressBar1";
+            this.eC_ProgressBar1.Value = 50;
+            // 
+            // eC_TextBox1
+            // 
+            this.eC_TextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.eC_TextBox1.Location = new System.Drawing.Point(36, 122);
+            this.eC_TextBox1.MaximumSize = new System.Drawing.Size(2147483647, 20);
+            this.eC_TextBox1.MinimumSize = new System.Drawing.Size(100, 20);
+            this.eC_TextBox1.Multiline = false;
+            this.eC_TextBox1.Name = "eC_TextBox1";
+            this.eC_TextBox1.Size = new System.Drawing.Size(150, 20);
+            this.eC_TextBox1.TabIndex = 4;
+            this.eC_TextBox1.Text = "";
+            // 
+            // MainTheme
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(268, 327);
+            this.Controls.Add(this.eC_Theme1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "MainTheme";
+            this.Text = "Form1";
+            this.eC_Theme1.ResumeLayout(false);
+            this.eC_Tabs1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.eC_Panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private EC_Theme eC_Theme1;
+        private EC_Notification eC_Notification1;
+        private EC_Tabs eC_Tabs1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private EC_Panel eC_Panel1;
+        private EC_TextBox eC_TextBox1;
+        private EC_ProgressBar eC_ProgressBar1;
+        private EC_CheckBox eC_CheckBox2;
+        private EC_CheckBox eC_CheckBox1;
+        private EC_Button eC_Button1;
+        private System.Windows.Forms.TabPage tabPage2;
+    }
+}
+
